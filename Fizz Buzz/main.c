24e6581738260c56328725ff670aa547f2946909
Suziewan <angielinton@me.com>
2016-12-14 23:20:33 -0500

    First Commit

--- Fizz Buzz/main.c
+++ Fizz Buzz/main.c
@@ -9,7 +9,23 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int i;
+    for(i=1; i<=100; i++)
+    {
+        if (i % 3 == 0)
+        printf("%d Fizz\n", i);
+        
+        if (i % 5 == 0)
+            printf("%d Buzz\n", i);
+        
+        if ((i % 5 == 0) && (i % 3 == 0))
+            printf("%d FizzBuzz\n", i);
+    
+    
+    else
+        printf("%d \n", i );
+    }
+
     return 0;
 }
